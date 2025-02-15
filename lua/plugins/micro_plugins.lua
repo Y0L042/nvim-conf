e002b20dc5d3c92113bec2d5a24949eaa4f78fbf
Y0L042@DinkBoekUCRT64 <vlamfletcher@gmail.com>
2025-02-15 15:16:18 +0200

    add autoclose

--- lua/plugins/micro_plugins.lua
+++ lua/plugins/micro_plugins.lua
@@ -1,4 +1,17 @@
 return {
+	{
+		'm4xshen/autoclose.nvim',
+        event = "VeryLazy",
+		config = function()
+			require("autoclose").setup({
+			   keys = {
+			   },
+			})        
+			--vim.api.nvim_set_keymap('i', '/*', '/*  */<Left><Left><Left>', { noremap = true, silent = true })
+
+		end,
+	},
+
     {
 		'tpope/vim-surround',
         event = "VeryLazy",
